b028401bdec59b84a8ace4923d82c03fe6a296cc
Abraham José González Barboza <jgab.net@gmail.com>
2015-04-27 16:26:11 -0430

    agregado modal con font jedi

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ $(function () {
     	slide: function (event, ui) {
     		$('.image-old').css('opacity', 1 - ui.value*0.010);
     		$('.image-now').css('opacity', 0 + ui.value*0.010);
+
+    		if (ui.value == 100) {
+    			$('#greet').modal({show:true});
+    		}
     	}
     });
 
